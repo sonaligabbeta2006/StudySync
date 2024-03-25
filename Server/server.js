47d68efd570ce41febdebb32ee13672c6f03833b
sonali_2006 <125039066+sonaligabbeta2006@users.noreply.github.com>
2024-03-25 15:56:15 +0530

    minor correction

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,9 @@
+import connectionToDB from "./config/dbConnection.js";
+import app from "./app.js";
 
-import connectionToDB from './config/dbConnection.js';
-import app from './app.js';
+const PORT = process.env.PORT || 5000;
 
-const PORT= process.env.PORT || 5000;
-
-
-app.listen(PORT,async ()=>{
-    await connectionToDB();
-    console.log(`App is running at  http:localhost:${PORT} `);
-})
+app.listen(PORT, async () => {
+  await connectionToDB();
+  console.log(`App is running at  http:localhost:${PORT} `);
+});
