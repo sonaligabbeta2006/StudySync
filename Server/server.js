357d57ac76affa12ad3fac01fd5fcb199502aef9
Varshinigutla <varshinigutla@gmail.com>
2024-03-26 09:51:12 +0530

    changes made

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,18 @@
+import cloudinary from 'cloudinary';
+
+
 import connectionToDB from "./config/dbConnection.js";
 import app from "./app.js";
 
+
 const PORT = process.env.PORT || 5000;
+
+cloudinary.v2.config({
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+  api_key:process.env.CLOUDINARY_API_SECRET,
+  api_secret:process.env.CLOUDINARY_API_SECRET,
+  secure:process.env.CLOUDINARY_SECURE,
+})
 
 app.listen(PORT, async () => {
   await connectionToDB();
