5b6730905fbec0f05b1a7ba7fdd187ce7a4c8fc8
Varshinigutla <varshinigutla@gmail.com>
2024-04-08 10:47:18 +0530

    some changes are made

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -2,11 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import AuthSliceReducer from "./Slices/AuthSlice";
 import CourseSliceReducer from "./Slices/CourseSlice";
+import StatReducer from "./Slices/StatSlice";
+import RazorpayReducer from "./Slices/RazorpaySlice";
+import LecturesReducer from "./Slices/LectureSlice";
 
 const store = configureStore({
   reducer: {
     auth: AuthSliceReducer,
     course: CourseSliceReducer,
+    razorpay: RazorpayReducer,
+    lecture:LecturesReducer,
+    stat:StatReducer,
   },
   devTools: true,
 });
